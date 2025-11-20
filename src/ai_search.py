cce4e83dd55e305515741e2c527dafc142801e62
jakubbor <jakub.borek@sutdent.up.krakow.pl>
2025-11-20 01:52:14 +0100

    added pandas, added priority verification, added a second request if the first one fails to pass parsing/verification

--- src/ai_search.py
+++ src/ai_search.py
@@ -1,5 +1,6 @@
 import logging
 import json
+import pandas
 from api_handler import ApiHandler
 
 
@@ -9,21 +10,21 @@ class AISearch():
         self.api_handler = ApiHandler()
 
 
-    def parseMsg(self, msg):
+    def parse_msg(self, msg):
         parsed = None
         try:
             parsed = json.loads(msg)
         except Exception as X:
             logging.getLogger(__name__).info("Parsing failed at first clean try, running advanced parsing measures")
 
-            parsedMsg = msg
-            if '```json' in parsedMsg:
+            parsed_msg = msg
+            if '```json' in parsed_msg:
                 logging.getLogger(__name__).info("Parser: Replacing \`\`\`json")
-                parsedMsg = parsedMsg.replace("```json", "")
-                parsedMsg = parsedMsg.replace("```", "")
+                parsed_msg = parsed_msg.replace("```json", "")
+                parsed_msg = parsed_msg.replace("```", "")
                 
-            logging.getLogger(__name__).info(parsedMsg)
-            parsed = json.loads(parsedMsg)
+            logging.getLogger(__name__).info(parsed_msg)
+            parsed = json.loads(parsed_msg)
 
 
         logging.getLogger(__name__).info("Message after parsing:")
@@ -31,14 +32,42 @@ class AISearch():
 
         return parsed
 
+    def verify_values(self, msg):
+        for idx, item in enumerate(msg):
+            checked_val = int(msg[item]) # check if it's a number
+            # check if the value is in correct bonds (0-100)
+            if checked_val >= 0 and checked_val <= 100:
+                continue
+            else:
+                raise Exception("Value was not in expected bounds:", msg[item])
+
+        logging.getLogger(__name__).info("Values verified")
+        return msg
 
 
-    def search(self, query, db, model):
+
+    def get_response(self, query, db, model):
         response = self.api_handler.send_request(query, db, "huggingface", model)
         msg = response.choices[0].message.content
         logging.getLogger(__name__).info(msg)
 
-        msg = self.parseMsg(msg)
+        return msg
+
+
+    def search(self, query, db, model):
+        msg = self.get_response(query, db, model)
+
+        try:
+            msg = self.parse_msg(msg)
+            msg = self.verify_values(msg)
+        except Exception as X:
+            logging.getLogger(__name__).error(X)
+
+            #try 2nd time if first request failed
+            msg = self.get_response(query, db, model)
+            msg = self.parse_msg(msg)
+            msg = self.verify_values(msg)
+
         return msg
 
 
